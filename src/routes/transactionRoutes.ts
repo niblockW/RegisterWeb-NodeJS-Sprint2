7b32d461ad7c45ed2b851c033d0110536a60885e
William Smith <wws002@email.uark.edu>
2019-04-09 14:29:19 -0500

    fixing comment errors

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -1,4 +1,4 @@
-import express from "express"; //appears on every other route file
+import express from "express"; // appears on every other route file
 import { RouteLookup } from "../controllers/lookups/stringLookup";
 // import * as TransactionController from "../controllers/transactionController"; // NEED TO CREATE THIS FILE
 
